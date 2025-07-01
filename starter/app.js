82367d64a8349520dce9f654645450a7b64ca8f7
Deep-2108 <deepa210805@gmail.com>
2025-07-01 14:09:22 +0530

    handling url parameters

--- starter/app.js
+++ starter/app.js
@@ -1,10 +1,51 @@
 const express = require('express');
-
+const fs=require('fs');
 const app=express();
+app.use(express.json());
+// app.get('/',(req,res)=>{
 
-app.get('/',(req,res)=>{
-    
-});
+//     res.status(200).json({message:'hello from the server side',app:'Natours'});
+// });
+
+// app.post('/',(req,res)=>{
+//     res.send('you can post to this url ');
+// })
+
+const tours=JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours',(req,res)=>{
+    res.status(200).json({
+        status:'success',
+        data: {
+            tours
+        }
+    })
+})
+app.get('/api/v1/tours/:id',(req,res)=>{
+    console.log(req.params);
+    res.status(200).json({
+        status:'success',
+        // data: {
+        //     tours
+        // }
+    })
+})
+
+app.post('/api/v1/tours',(req,res)=>{
+    // console.log(req.body);
+    const newId=tours[tours.length-1].id+1;
+    const newTour=Object.assign({id:newId},req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err=>{
+            res.status(201).json({
+                status: "success",
+                data : {
+                    tour :newTour
+                }
+            })
+    })
+    // res.send('Done');
+})
 
 const port=3000;
 app.listen(port,()=>{
